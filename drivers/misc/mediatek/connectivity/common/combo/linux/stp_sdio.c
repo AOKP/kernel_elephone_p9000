30dc50aa5c5e19931b93a66c3e4db4f1af2d467e
David Chu <david.chu@mediatek.com>
2016-05-19 12:29:10 -0700

    A2DP performance improvement.
    Replaced busy udelay loop with usleep_range
    to reduce CPU usage in stp_sdio_tx_rx.
    
    Bug: 27713674

--- drivers/misc/mediatek/connectivity/common/combo/linux/stp_sdio.c
+++ drivers/misc/mediatek/connectivity/common/combo/linux/stp_sdio.c
@@ -503,11 +503,11 @@ static INT32 stp_sdio_do_own_clr(INT32 wait)
 			/* 4 <2> handle ownership back interrupt */
 			STPSDIO_DBG_FUNC("firmware ownback is polled!(%d)\n",
 					 CLR_OWN_RETRY - retry);
-			/* udelay(2000); */
+			usleep_range(2000,2000);
 			break;
 		}
 		STPSDIO_DBG_FUNC("firmware ownback is no polled, wait for (%d us) and retry\n", delay_us);
-		udelay(delay_us);
+		usleep_range(delay_us,delay_us);
 		if (0 == (retry - 1)%40)
 			STPSDIO_ERR_FUNC("own back failed in %d us, something might goes wrong\n", 40*delay_us);
 
