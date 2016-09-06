84475d65ed49c10ede96933b32380ca334bfee4d
Jonathon Fitch <lightman1@virginmedia.com>
2016-09-06 12:55:42 +0100

    Update build scripts

--- makeconfig.sh
+++ makeconfig.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -x
 make clean && make mrproper
 export CROSS_COMPILE=~/android/android-ndk-r12b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 export ARCH=arm64
 export KERNEL_CONFIG=/arch/arm64/configs/DarkSense_defconfig
-make ARCH=arm64 DarkSense_defconfig
+make -j8 DarkSense_defconfig
--- makekernel.sh
+++ makekernel.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-set -x
 make clean && make mrproper
 export CROSS_COMPILE=~/android/android-ndk-r12b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 export ARCH=arm64
-export SUB_ARCH=arm64
 export KERNEL_CONFIG=/arch/arm64/configs/DarkSense_defconfig
-make ARCH=arm64 DarkSense_defconfig
-make -j6 ARCH=arm64
+make -j8 DarkSense_defconfig
+make -j8
