4788627f73e950240e6c73a3ad7dd4b88fa631b1
Jonathon Fitch <lightman1@virginmedia.com>
2016-09-14 23:21:15 +0100

    build scripts: reorganise build scripts

--- makekernel.sh
+++ makekernel.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-rm DarkSense
+DEFCONFIG=DarkSense_defconfig
+KERNEL=DarkSense
+if [  -f $KERNEL ]; then
+	rm $KERNEL
+fi
+if [  -f $DEFCONFIG ]; then
+	rm $DEFCONFIG
+fi
 make clean && make mrproper
 export CROSS_COMPILE=~/android/android-ndk-r12b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 export ARCH=arm64
-export KERNEL_CONFIG=/arch/arm64/configs/DarkSense_defconfig
-make -j8 DarkSense_defconfig
+export KERNEL_CONFIG=/arch/arm64/configs/$DEFCONFIG
+make -j8 $DEFCONFIG
 make -j8
-cp arch/arm64/boot/Image.gz-dtb DarkSense
+cp arch/arm64/boot/Image.gz-dtb $KERNEL
