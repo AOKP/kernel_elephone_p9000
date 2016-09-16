a0244e9d4f4641b1aa07cc349a8d5e9cbb5793b0
Jonathon Fitch <lightman1@virginmedia.com>
2016-09-16 06:25:47 +0100

    build scripts: Remove some uneeded code

--- makeconfig.sh
+++ makeconfig.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 DEFCONFIG=DarkSense_defconfig
-KERNEL=DarkSense
 make clean && make mrproper
 export CROSS_COMPILE=~/android/android-ndk-r12b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 export ARCH=arm64
-export KERNEL_CONFIG=/arch/arm64/configs/$DEFCONFIG
 make -j8 $DEFCONFIG
--- makekernel.sh
+++ makekernel.sh
@@ -10,7 +10,6 @@ fi
 make clean && make mrproper
 export CROSS_COMPILE=~/android/android-ndk-r12b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 export ARCH=arm64
-export KERNEL_CONFIG=/arch/arm64/configs/$DEFCONFIG
 make -j8 $DEFCONFIG
 make -j8
 cp arch/arm64/boot/Image.gz-dtb $KERNEL
