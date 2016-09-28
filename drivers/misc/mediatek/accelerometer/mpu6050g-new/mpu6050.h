7cd8f56d0ca7993cef7268b0d0e51042fb21217f
Jonathon Fitch <lightman1@virginmedia.com>
2016-09-28 19:45:07 +0100

    Swap out Shipped drivers with ones from Ulefone Future

--- drivers/misc/mediatek/accelerometer/mpu6050g-new/mpu6050.h
+++ drivers/misc/mediatek/accelerometer/mpu6050g-new/mpu6050.h
@@ -20,6 +20,7 @@
 
 /* register Value */
 #define MPU6050_PWRM2_ACCEL_DISABLE		0x38
+#define MPU6050_FIXED_DEVID         0x68	/* or 0x69 */
 
 					   /* delay(ms) */
 #define MPU6050_BW_260HZ            0x00	/* 0 */
--- drivers/misc/mediatek/leds/mt6755/leds_hal.h
+++ drivers/misc/mediatek/leds/mt6755/leds_hal.h
@@ -56,5 +56,4 @@ extern int mt_mt65xx_blink_set(struct led_classdev *led_cdev,
 extern struct cust_mt65xx_led *mt_get_cust_led_list(void);
 
 extern int mtkfb_set_backlight_level(unsigned int level);
-
 #endif
