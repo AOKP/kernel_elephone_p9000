8794ebb2b45373f5cb1f5b97646405b396f65e8f
Jonathon Fitch <lightman1@virginmedia.com>
2016-11-28 08:22:11 +0000

    Update Android NDK to R13B

--- makeconfig.sh
+++ makeconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DEFCONFIG=DarkSense_defconfig
 make clean && make mrproper
-export CROSS_COMPILE=~/android/android-ndk-r12b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/android/android-ndk-r13b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 export ARCH=arm64
 make -j8 $DEFCONFIG
