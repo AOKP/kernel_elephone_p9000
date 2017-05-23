99e40ffee3d82f66b9a65443f3f11afa50ac1cd1
leskal <alexakis1997@gmail.com>
2017-05-23 14:13:52 +0300

    Regenerate config
    * Update location folder of ndk in scripts

--- makeconfig.sh
+++ makeconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DEFCONFIG=lineage_p9000_defconfig
 make clean && make mrproper
-export CROSS_COMPILE=~/android/android-ndk-r13b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/android/android-ndk-r14b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 export ARCH=arm64
 make -j8 $DEFCONFIG
