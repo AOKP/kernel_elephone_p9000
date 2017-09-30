1a028bf412acc34cf9c3c1e2d2699eb451015c5a
Mister Oyster <oysterized@gmail.com>
2017-09-30 16:24:38 +0200

    mtk: devinfo: update from N

--- drivers/misc/mediatek/devinfo/devinfo.c
+++ drivers/misc/mediatek/devinfo/devinfo.c
@@ -18,6 +18,7 @@
 #include <linux/proc_fs.h>
 #include <linux/cdev.h>
 #include <linux/mm.h>
+#include <linux/slab.h>
 #include <asm/io.h>
 #include <asm/uaccess.h>
 #include <linux/ioctl.h>
@@ -25,68 +26,96 @@
 #ifdef CONFIG_OF
 #include <linux/of_fdt.h>
 #endif
+#include <linux/atomic.h>
 #include <asm/setup.h>
 #include "devinfo.h"
 
-u32 g_devinfo_data[DEVINFO_MAX_SIZE];
+enum {
+	DEVINFO_UNINIT = 0,
+	DEVINFO_INITIALIZING_OR_INITIALIZED = 1
+} DEVINFO_INIT_STATE;
+
+static u32 *g_devinfo_data;
+static u32 g_devinfo_size;
 static struct cdev devinfo_cdev;
 static struct class *devinfo_class;
 static dev_t devinfo_dev;
+static atomic_t g_devinfo_init_status = ATOMIC_INIT(DEVINFO_UNINIT);
+static atomic_t g_devinfo_init_errcnt = ATOMIC_INIT(0);
 /*****************************************************************************
 *FUNCTION DEFINITION
 *****************************************************************************/
 static int devinfo_open(struct inode *inode, struct file *filp);
 static int devinfo_release(struct inode *inode, struct file *filp);
 static long devinfo_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+static void init_devinfo_exclusive(void);
 
 /**************************************************************************
 *EXTERN FUNCTION
 **************************************************************************/
 u32 devinfo_get_size(void)
 {
-	u32 data_size = 0;
-
-	data_size = ARRAY_SIZE(g_devinfo_data);
-	return data_size;
+	return g_devinfo_size;
 }
+EXPORT_SYMBOL(devinfo_get_size);
+
+u32 devinfo_ready(void)
+{
+	if (devinfo_get_size() > 0)
+		return 1;
+	return 0;
+}
+EXPORT_SYMBOL(devinfo_ready);
 
 u32 get_devinfo_with_index(u32 index)
 {
 	int size = devinfo_get_size();
 	u32 ret = 0;
 
-	if ((index >= 0) && (index < size))
+#ifdef CONFIG_OF
+	if (size == 0) {
+		/* Devinfo API users may call this API earlier than devinfo data is ready from dt. */
+		/* If the earlier API users found, make the devinfo data init earlier at that time. */
+		init_devinfo_exclusive();
+		size = devinfo_get_size();
+	}
+#endif
+
+	if (((index >= 0) && (index < size)) && (g_devinfo_data != NULL))
 		ret = g_devinfo_data[index];
 	else {
-		pr_warn("devinfo data index out of range:%d\n", index);
-		pr_warn("devinfo data size:%d\n", size);
+		pr_err("devinfo data index out of range:%d\n", index);
+		pr_err("devinfo data size:%d\n", size);
+		pr_err("USE devinfo_ready() API TO CHECK IF DEVINFO DATA IS READY FOR READING\n");
 		ret = 0xFFFFFFFF;
 	}
 
 	return ret;
 }
+EXPORT_SYMBOL(get_devinfo_with_index);
+
 /**************************************************************************
 *STATIC FUNCTION
 **************************************************************************/
 
 static const struct file_operations devinfo_fops = {
-	.open = devinfo_open,
-	.release = devinfo_release,
-	.unlocked_ioctl   = devinfo_ioctl,
+	    .open = devinfo_open,
+	    .release = devinfo_release,
+	    .unlocked_ioctl   = devinfo_ioctl,
 #ifdef CONFIG_COMPAT
-	.compat_ioctl = devinfo_ioctl,
+	    .compat_ioctl = devinfo_ioctl,
 #endif
-	.owner = THIS_MODULE,
+	    .owner = THIS_MODULE,
 };
 
 static int devinfo_open(struct inode *inode, struct file *filp)
 {
-	return 0;
+	   return 0;
 }
 
 static int devinfo_release(struct inode *inode, struct file *filp)
 {
-	return 0;
+	   return 0;
 }
 
 /**************************************************************************
@@ -97,8 +126,8 @@ static long devinfo_ioctl(struct file *file, unsigned int cmd, unsigned long arg
 	u32 index = 0;
 	int err   = 0;
 	int ret   = 0;
-	u32 data_size = ARRAY_SIZE(g_devinfo_data);
 	u32 data_read = 0;
+	int size = devinfo_get_size();
 
 	/* ---------------------------------- */
 	/* IOCTL							  */
@@ -121,12 +150,12 @@ static long devinfo_ioctl(struct file *file, unsigned int cmd, unsigned long arg
 	case READ_DEV_DATA:
 		if (copy_from_user((void *)&index, (void __user *)arg, sizeof(u32)))
 			return -1;
-		if (index < data_size) {
+		if (index < size) {
 			data_read = get_devinfo_with_index(index);
 			ret = copy_to_user((void __user *)arg, (void *)&(data_read), sizeof(u32));
 		} else {
 			pr_warn("%s Error! Data index larger than data size. index:%d, size:%d\n", MODULE_NAME,
-			index, data_size);
+				index, size);
 			return -2;
 		}
 		break;
@@ -200,7 +229,6 @@ static int __init devinfo_init(void)
 static int __init devinfo_parse_dt(unsigned long node, const char *uname, int depth, void *data)
 {
 	struct devinfo_tag *tags;
-	int i;
 	u32 size = 0;
 
 	if (depth != 1 || (strcmp(uname, "chosen") != 0 && strcmp(uname, "chosen@0") != 0))
@@ -208,27 +236,45 @@ static int __init devinfo_parse_dt(unsigned long node, const char *uname, int de
 
 	tags = (struct devinfo_tag *) of_get_flat_dt_prop(node, "atag,devinfo", NULL);
 	if (tags) {
-		size = tags->data_size;
-		if (size == DEVINFO_MAX_SIZE)
-			for (i = 0; i < size; i++)
-				g_devinfo_data[i] = tags->data[i];
-		else {
-			for (i = 0; i < 50; i++)
-				pr_err("[ERROR][devinfo size mismatch] devinfo size:%d, atag size:%d\n",
-					DEVINFO_MAX_SIZE, size);
-			for (i = 0; i < DEVINFO_MAX_SIZE; i++)
-				g_devinfo_data[i] = (u32)0;
-		}
-		/* print chip id for debugging purpose */
-		pr_debug("tag_devinfo_data size:%d\n", size);
+		size = (tags->size) - (u32)(devinfo_lk_atag_tag_size(devinfo_lk_atag_tag_devinfo_data));
+
+		g_devinfo_data = kmalloc(sizeof(struct devinfo_tag) + (size * sizeof(u32)), GFP_KERNEL);
+		g_devinfo_size = size;
+
+		WARN_ON(size > 300); /* for size integer too big protection */
+
+		memcpy(g_devinfo_data, tags->data, (size * sizeof(u32)));
+
+		pr_err("devinfo size:%d, tag size(devinfo_lk_atag_tag_devinfo_data):%d\n",
+			size, (u32)(devinfo_lk_atag_tag_size(devinfo_lk_atag_tag_devinfo_data)));
+	} else {
+		pr_err("'atag,devinfo' is not found\n");
 	}
 
 	return 1;
 }
 
+static void init_devinfo_exclusive(void)
+{
+	if (DEVINFO_INITIALIZING_OR_INITIALIZED == atomic_read(&g_devinfo_init_status)) {
+		atomic_inc(&g_devinfo_init_errcnt);
+		pr_err("devinfo data already init done earlier. 'init_devinfo_exclusive' extra times: %d\n",
+			atomic_read(&g_devinfo_init_errcnt));
+		return;
+	}
+
+	if (DEVINFO_UNINIT == atomic_read(&g_devinfo_init_status))
+		atomic_set(&g_devinfo_init_status, DEVINFO_INITIALIZING_OR_INITIALIZED);
+	else
+		return;
+
+	of_scan_flat_dt(devinfo_parse_dt, NULL);
+}
+
 static int __init devinfo_of_init(void)
 {
-	of_scan_flat_dt(devinfo_parse_dt, NULL);
+	init_devinfo_exclusive();
+
 	return 0;
 }
 #endif
@@ -260,5 +306,3 @@ early_initcall(devinfo_of_init);
 module_init(devinfo_init);
 module_exit(devinfo_exit);
 MODULE_LICENSE("GPL");
-
-
