5e0447cf688391e9768e97fed6b2b2909649fd64
tribual <tribual@web.de>
2018-02-26 14:57:22 +0100

    RC1

--- makekernel.sh
+++ makekernel.sh
@@ -8,7 +8,7 @@ if [  -f $DEFCONFIG ]; then
 	rm $DEFCONFIG
 fi
 make clean && make mrproper
-export CROSS_COMPILE=~/android/android-ndk-r14b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/android/android-ndk-r13b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 export ARCH=arm64
 make -j8 $DEFCONFIG
 make -j8
